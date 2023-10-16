08c57ab20d15bc2a9d2f4fdf234dff7699e986ae
Farhanrezy <farhanrezy70@gmail.com>
2023-10-16 09:47:07 +0700

    update perulangan

--- Ekspedisi.java
+++ Ekspedisi.java
@@ -1,83 +1,82 @@
 import java.util.Scanner;
 
 public class Ekspedisi {
-    
-    public static void main(String [] args){
-        
+
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
- 
+
         String Namabarang, Tujuan, tambahkemasan, packing;
-        int ongkosberat, totalbiaya , Beratbarang, jasapengiriman, ongkir, kemasan;
+        int ongkosberat, totalbiaya, Beratbarang, biayaPengiriman, ongkir, kemasan;
         int kg1 = 10000, kg2 = 20000, kg3 = 50000, Express = 30000, Reguler = 10000, Kargo = 20000;
 
-        System.out.println("Masukkan nama barang : ");
+        System.out.print("Masukkan nama barang : ");
         Namabarang = input.nextLine();
-        System.out.println("Masukkan Alamat : ");
+        System.out.print("Masukkan Alamat : ");
         Tujuan = input.nextLine();
-        
+
         System.out.print("Berat Barang yang dikirim = ");
         Beratbarang = input.nextInt();
-
-        if (Beratbarang <= 2){
+        
+        if (Beratbarang <= 2) {
             System.out.println("Ongkos berat pengiriman = " + kg1);
             ongkosberat = kg1 * Beratbarang;
-        }else if (Beratbarang <= 5){
+        } else if (Beratbarang <= 5) {
             System.out.println("Ongkos berat pengiriman = " + kg2);
             ongkosberat = kg2 * Beratbarang;
-        }else {
+        } else {
             System.out.println("Ongkos berat pengiriman = " + kg3);
             ongkosberat = kg3 * Beratbarang;
         }
-        System.out.println("total ongkos berat barang = " + ongkosberat );
+        System.out.println("total ongkos berat barang = " + ongkosberat);
 
-        System.out.print("Pilih Jenis Pengiriman ( 1 = Express, 2 = Reguler, 3 = Kargo ) : ");
-        jasapengiriman = input.next().charAt(0);
-        
-        switch (jasapengiriman){
-            case '1':
-            System.out.println("Harga Pengiriman Express : "+Express);
-            jasapengiriman = ongkosberat + Express;
-            break;
-            case '2' :
-            System.out.println("Harga Pengiriman Reguler : "+Reguler);
-            jasapengiriman = ongkosberat + Reguler;
-            break;
-            case '3' : 
-            System.out.println("Harga Pengiriman Kargo : "+Kargo);
-            jasapengiriman = ongkosberat + Kargo;
-            break;
+        boolean pilihan = true;
+        while (pilihan) {
+            System.out.print("Pilih jenis pengiriman  (1. Express, 2. Reguler, 3. Kargo): ");
+            biayaPengiriman = input.nextInt();
+            switch (biayaPengiriman) {
+                case 1:
+                    System.out.println("Harga Pengiriman Express : " + Express);
+                    biayaPengiriman = ongkosberat + Express;
+                    break;
+                case 2:
+                    System.out.println("Harga Pengiriman Reguler : " + Reguler);
+                    biayaPengiriman = ongkosberat + Reguler;
+                    break;
+                case 3:
+                    System.out.println("Harga Pengiriman Kargo : " + Kargo);
+                    biayaPengiriman = ongkosberat + Kargo;
+                    break;
+            }
+            System.out.print("apakah barang sudah dikemas : ");
+            packing = input.next();
+
+            if (packing.equalsIgnoreCase("belum")) {
+                if (Beratbarang <= 2)
+                    kemasan = 5000;
+                else if (Beratbarang <= 5)
+                    kemasan = 10000;
+                else
+                    kemasan = 20000;
+            } else {
+                kemasan = 0;
+            }
+
+            ongkir = biayaPengiriman + kemasan;
+
+            System.out.println("---------------------------");
+            System.out.println("Nama Barang : " + Namabarang);
+            System.out.println("Tujuan Pengiriman : " + Tujuan);
+            System.out.println("Total biaya pengiriman = " + ongkir);
+
+            System.out.println("---------------------------");
+            System.out.println("Apakah anda ingin menghitung biaya pengiriman lagi? (ya/tidak): ");
+            String lanjut = input.next();
+            if (lanjut.equalsIgnoreCase("tidak")) {
+                pilihan = false;
+            }
+            //input.nextLine();
         }
-
-        System.out.println("apakah barang sudah dikemas : ");
-        packing = input.next();
-
-        if (packing.equalsIgnoreCase("belum")) {
-            if (Beratbarang <= 2) 
-                kemasan = 5000;
-            else if (Beratbarang <= 5) 
-                kemasan = 10000;
-            else
-                kemasan = 20000;
-        }else {
-           kemasan = 0;
-        }
-       
-        
-        ongkir = jasapengiriman + kemasan; 
-        
-        System.out.println("---------------------------");        
-        System.out.println("Nama Barang : " + Namabarang );
-        System.out.println("Tujuan Pengiriman : " + Tujuan );
-        System.out.println("Total biaya pengiriman = " + ongkir );
-
-        System.out.println("---------------------------");       
-        System.out.println("Mulai pengiriman:");
-
-        for (int i = 0; i < daftarPesanan.length; i++) {
-            String pesanan = daftarPesanan[i];
-            // Proses pesanan di sini
-            System.out.println("Mengirim pesanan: " + pesanan);
-        }
-        System.out.println("Pengiriman selesai.");
+        System.out.println("Terima Kasih");
     }
 }
